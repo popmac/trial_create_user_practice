5967af7c9f65e6d7dda6676e480030a4df0a7628
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-05 23:38:42 +0900

    ログイン機能を追加した。

--- app/controllers/staff/sessions_controller.rb
+++ app/controllers/staff/sessions_controller.rb
@@ -9,4 +9,21 @@ class Staff::SessionsController < Staff::Base
     end
   end
 
+  def create
+    @form = Staff::LoginForm.new(staff_login_form_params)
+    if @form.email.present?
+      staff_member = StaffMember.find_by(email_for_index: @form.email.downcase)
+    end
+    if staff_member
+      session[:staff_member_id] = staff_member.id
+      redirect_to :staff_root
+    else
+      render action: 'new'
+    end
+  end
+
+  private
+  def staff_login_form_params
+    params.require(:staff_login_form).permit(:email, :password)
+  end
 end
