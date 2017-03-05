3e08844fba092f67f0742be5ed8c1af9fd8e22b9
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-05 20:05:44 +0900

    403 Forbiddenの発生時の処理を追加した。

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,12 @@ class ApplicationController < ActionController::Base
 
   layout :set_layout
 
+  class Forbidden < ActionController::ActionControllerError; end
+  class IpAdressRejected < ActionController::ActionControllerError; end
+
   rescue_from Exception, with: :rescue500
+  rescue_from Forbidden, with: :rescue403
+  rescue_from IpAdressRejected, with: :rescue403
 
   private
   def set_layout
@@ -14,6 +19,11 @@ class ApplicationController < ActionController::Base
     else
       'customer'
     end
+  end
+
+  def rescue403(e)
+    @exception = e
+    render 'errors/forbidden', status: 403
   end
 
   def rescue500(e)
