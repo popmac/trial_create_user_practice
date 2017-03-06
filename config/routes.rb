a624d0767c4af36d6ddfaee0b0868a81b62b37c3
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-06 16:00:12 +0900

    sessionのcreateとdestoryを単一リソースにリファクタリングした。

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,13 @@ Rails.application.routes.draw do
   namespace :staff, path: '' do
     root 'top#index'
     get 'login' => 'sessions#new', as: :login
-    post 'session' => 'sessions#create', as: :session
-    delete 'session' => 'sessions#destroy'
+    resource :session, only: [:create, :destroy], as: :session
   end
 
   namespace :admin do
     root 'top#index'
     get 'login' => 'sessions#new', as: :login
-    post 'session' => 'sessions#create', as: :session
-    delete 'session' => 'sessions#destroy'
+    resource :session, only: [:create, :destroy], as: :session
     resources :staff_members
   end
 
