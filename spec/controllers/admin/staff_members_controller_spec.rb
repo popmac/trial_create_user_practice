31b810b0a54f0c182618b2a67a20b13580e02cc7
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-06 20:56:40 +0900

    admin/staff_membersコントローラーのcreateアクションのテストを追加した。

--- spec/controllers/admin/staff_members_controller_spec.rb
+++ spec/controllers/admin/staff_members_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Admin::StaffMembersController, type: :controller do
+  describe '#create' do
+    example '職員一覧ページにリダイレクト' do
+      post :create, params: { staff_member: attributes_for(:staff_member) }
+      expect(response).to redirect_to(admin_staff_members_url)
+    end
 
+    example '例外ActionController::ParameterMissingが発生' do
+      bypass_rescue
+      expect { post :create, params: nil }.to raise_error(ActionController::ParameterMissing)
+    end
+  end
 end
