21a3d7c7de3353db688988a68ca9df7d277cc78a
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-06 20:58:50 +0900

    リファクタリングをした。本の内容とは関係ないもの。

--- app/controllers/admin/staff_members_controller.rb
+++ app/controllers/admin/staff_members_controller.rb
@@ -46,6 +46,6 @@ class Admin::StaffMembersController < Admin::Base
 
   private
   def staff_member_params
-    params.require(:staff_member).permit(:email, :password, :family_name, :given_name, :family_name_kana, :given_name_kana, :start_date, :end_date, :susupended)
+    params.require(:staff_member).permit(:email, :password, :family_name, :given_name, :family_name_kana, :given_name_kana, :start_date, :end_date, :suspended)
   end
 end
