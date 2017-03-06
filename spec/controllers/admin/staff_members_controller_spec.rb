52bea13739eac138d4f0cf76b893333655d7c68a
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-06 21:28:41 +0900

    admin/staff_membersコントローラーのcreateアクションのテストをリファクタリングした。

--- spec/controllers/admin/staff_members_controller_spec.rb
+++ spec/controllers/admin/staff_members_controller_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Admin::StaffMembersController, type: :controller do
+
+  let(:staff_member_params) { attributes_for(:staff_member) }
+
   describe '#create' do
     example '職員一覧ページにリダイレクト' do
-      post :create, params: { staff_member: attributes_for(:staff_member) }
+      post :create, params: { staff_member: staff_member_params }
       expect(response).to redirect_to(admin_staff_members_url)
     end
 
