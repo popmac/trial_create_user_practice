fdf41d023e2146873f836537778df3ce5813f656
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-06 18:11:20 +0900

    admin/staff_membersコントローラーのcreateアクションを追加した。

--- app/controllers/admin/staff_members_controller.rb
+++ app/controllers/admin/staff_members_controller.rb
@@ -7,6 +7,16 @@ class Admin::StaffMembersController < Admin::Base
     @staff_member = StaffMember.new
   end
 
+  def create
+    @staff_member = StaffMember.new(staff_member_params)
+    if @staff_member.save
+      flash.notice = '職員アカウントを新規登録しました'
+      redirect_to :admin_staff_members
+    else
+      render action: 'new'
+    end
+  end
+
   def show
     staff_member = StaffMember.find(params[:id])
     redirect_to [:edit, :admin, staff_member]
@@ -14,5 +24,10 @@ class Admin::StaffMembersController < Admin::Base
 
   def edit
     @staff_member = StaffMember.find(params[:id])
+  end
+
+  private
+  def staff_member_params
+    params.require(:staff_member).permit(:email, :family_name, :given_name, :family_name_kana, :given_name_kana, :start_date)
   end
 end
