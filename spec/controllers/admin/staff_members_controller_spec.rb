96535ebda6bdb059ab83d229a2ced40e4bda53e1
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-06 21:36:14 +0900

    本の内容に合わせるようにヘルパーメソッドの名前を修正した。

--- spec/controllers/admin/staff_members_controller_spec.rb
+++ spec/controllers/admin/staff_members_controller_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Admin::StaffMembersController, type: :controller do
 
-  let(:staff_member_params) { attributes_for(:staff_member) }
+  let(:params_hash) { attributes_for(:staff_member) }
 
   describe '#create' do
     example '職員一覧ページにリダイレクト' do
-      post :create, params: { staff_member: staff_member_params }
+      post :create, params: { staff_member: params_hash }
       expect(response).to redirect_to(admin_staff_members_url)
     end
 
@@ -20,17 +20,17 @@ RSpec.describe Admin::StaffMembersController, type: :controller do
     let(:staff_member) { create(:staff_member) }
 
     example 'suspendedフラグをセットする' do
-      staff_member_params.merge!(suspended: true)
-      patch :update, params: { id: staff_member.id, staff_member: staff_member_params }
+      params_hash.merge!(suspended: true)
+      patch :update, params: { id: staff_member.id, staff_member: params_hash }
       staff_member.reload
       expect(staff_member).to be_suspended
     end
 
     example 'hashed_passwordの値は書き換え不可' do
-      staff_member_params.delete(:password)
-      staff_member_params.merge!(hashed_password: 'x')
+      params_hash.delete(:password)
+      params_hash.merge!(hashed_password: 'x')
       expect {
-        patch :update, params: { id: staff_member.id, staff_member: staff_member_params }
+        patch :update, params: { id: staff_member.id, staff_member: params_hash }
       }.not_to change { staff_member.hashed_password.to_s }
     end
   end
