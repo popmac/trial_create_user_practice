149438e2801fda1b5877ff4a345d49d05d7d36af
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-07 23:23:16 +0900

    Adminに強制ログアウトの機能を追加した。そのテストを追加した。

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,5 +1,6 @@
 class Admin::Base < ApplicationController
   before_action :authorize
+  before_action :check_account
 
   private
   def current_administrator
@@ -16,6 +17,14 @@ class Admin::Base < ApplicationController
     unless current_administrator
       flash.notice = '管理者としてログインしてください'
       redirect_to :admin_login
+    end
+  end
+
+  def check_account
+    if current_administrator && current_administrator.suspended?
+      session.delete(:administrator_id)
+      flash.alert = 'アカウントが無効になりました'
+      redirect_to :admin_root
     end
   end
 end
--- spec/controllers/admin/top_controller_spec.rb
+++ spec/controllers/admin/top_controller_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Admin::TopController, type: :controller do
+  context 'ログイン後' do
 
+    let(:administrator) { create(:administrator) }
+
+    before do
+      session[:administrator_id] = administrator.id
+    end
+
+    describe '#index' do
+      example '通常はstaff/top/dashboard' do
+        get :index
+        expect(response).to render_template('admin/top/dashboard')
+      end
+
+      example '停止フラグがセットされたら強制的にログアウト' do
+        administrator.update_column(:suspended, true)
+        get :index
+        expect(session[:administrator_id]).to be_nil
+        expect(response).to redirect_to(admin_root_url)
+      end
+    end
+  end
 end
