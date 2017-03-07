3705cbbfc4adc187a4233a9867066f585fb37656
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-07 16:05:13 +0900

    管理者ページのアクセス制御を追加した。名前空間Adminに属するコントローラーのアクションでは原則としてauthorizeメソッドがアクションの前に実行されるようにした。ただし、admin/topとadmin/sessionsコントローラーではスキップするようにした。

--- app/controllers/admin/base.rb
+++ app/controllers/admin/base.rb
@@ -1,4 +1,6 @@
 class Admin::Base < ApplicationController
+  before_action :authorize
+
   private
   def current_administrator
     if session[:administrator_id]
@@ -9,4 +11,11 @@ class Admin::Base < ApplicationController
   # app/helpers/application_helper.rbに定義するのと同じ効果
   # ERBテンプレートの中で利用できるようになる
   helper_method :current_administrator
+
+  def authorize
+    unless current_administrator
+      flash.notice = '管理者としてログインしてください'
+      redirect_to :admin_login
+    end
+  end
 end
