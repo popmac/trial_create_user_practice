32163c0dd3a3d0013b5c6b45692d629e19c85dcc
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-08 01:14:05 +0900

    StaffMemberモデルとStaffEventモデルでアソシエーションを組んだ。

--- app/models/staff_event.rb
+++ app/models/staff_event.rb
@@ -1,2 +1,7 @@
 class StaffEvent < ApplicationRecord
+  # typeカラムから特別な意味が失われて、普通のカラムとして使用できる
+  self.inheritance_column = nil
+
+  belongs_to :member, class_name: 'StaffMember', foreign_key: 'staff_member_id'
+  alias_attribute :occurred_at, :created_at
 end
