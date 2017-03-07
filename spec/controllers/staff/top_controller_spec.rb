3e1505892b5a45ae0561b766ceae3a48719edca3
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-07 19:20:47 +0900

    強制的ログアウトのテストを追加した。

--- spec/controllers/staff/top_controller_spec.rb
+++ spec/controllers/staff/top_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Staff::TopController, type: :controller do
+  context 'ログイン後' do
 
+    let (:staff_member) { create(:staff_member) }
+
+    before do
+      session[:staff_member_id] = staff_member.id
+      session[:last_access_time] = 1.second.ago
+    end
+
+    describe '#index' do
+      example '通常はstaff/top/indexを表示' do
+        get :index
+        expect(response).to render_template('staff/top/index')
+      end
+
+      example '#停止フラグがセットされたら強制的にログアウト' do
+        staff_member.update_column(:suspended, true)
+        get :index
+        expect(session[:staff_member]).to be_nil
+        expect(response).to redirect_to(staff_root_url)
+      end
+    end
+  end
 end
