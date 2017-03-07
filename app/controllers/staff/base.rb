33c7a820cc10dedcc3694f7506cd9017907ef7f1
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-07 17:48:22 +0900

    セッションタイムアウトの機能を追加した。

--- app/controllers/staff/base.rb
+++ app/controllers/staff/base.rb
@@ -1,6 +1,7 @@
 class Staff::Base < ApplicationController
   before_action :authorize
   before_action :check_account
+  before_action :check_timeout
 
   private
   def current_staff_member
@@ -25,6 +26,23 @@ class Staff::Base < ApplicationController
       session.delete(:staff_member_id)
       flash.alert = 'アカウントが無効になりました'
       redirect_to :staff_root
+    end
+  end
+
+  TIMEOUT = 60.minutes
+
+  def check_timeout
+    if current_staff_member
+      # 最後にアクセスした時刻が現在から60分前の時刻よりも進んでいる場合
+      # つまり、最後にアクセスしてから60分以内の場合
+      if session[:last_access_time] >= TIMEOUT.ago
+        # 最終アクセス時刻を更新
+        session[:last_access_time] = Time.current
+      else
+        session.delete(:last_access_time)
+        flash.alert = 'セッションがタイムアウトしました'
+        redirect_to :staff_login
+      end
     end
   end
 end
