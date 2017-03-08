f592b369d0f98260bd451bc1a0757b822a9944ca
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-08 20:20:44 +0900

    admin/staff_eventsコントローラーのindexアクションとビューを追加した。

--- app/controllers/admin/staff_events_controller.rb
+++ app/controllers/admin/staff_events_controller.rb
@@ -1,2 +1,12 @@
-class Admin::StaffEventsController < ApplicationController
+class Admin::StaffEventsController < Admin::Base
+  # リソースがネストされている場合とされていない場合の両方に対応している
+  # ネストされているかどうかはstaff_member_idパラメーターに値がセットされているかどうかで判定
+  def index
+    if params[:staff_member_id]
+      @staff_member = StaffMember.find(params[:staff_member_id])
+      @events = @staff_member.events.order(occurred_at: :desc)
+    else
+      @events = StaffEvent.order(occurred_at: :desc)
+    end
+  end
 end
--- app/models/staff_event.rb
+++ app/models/staff_event.rb
@@ -4,4 +4,14 @@ class StaffEvent < ApplicationRecord
 
   belongs_to :member, class_name: 'StaffMember', foreign_key: 'staff_member_id'
   alias_attribute :occurred_at, :created_at
+
+  DESCRIPTION = {
+    logged_in: 'ログイン',
+    logged_out: 'ログアウト',
+    rejected: 'ログイン拒否'
+  }
+
+  def description
+    DESCRIPTION[type.to_sym]
+  end
 end
