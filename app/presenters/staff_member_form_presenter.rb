07f0a9a609d4eb561d276259d4f369dcdac5da2e
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-09 22:14:00 +0900

    リファクタリングをした。コードを削除し忘れていたため。

--- app/presenters/staff_member_form_presenter.rb
+++ app/presenters/staff_member_form_presenter.rb
@@ -9,8 +9,6 @@ class StaffMemberFormPresenter < FormPresenter
   def full_name_block(name1, name2, label_text, options = {})
     markup(:div, class: 'input-block') do |m|
       # optionsの中に:requiredキーが存在したならclass="required"を付与する
-      m << label(name1, label_text,
-        class: options[:required] ? 'required' : nil)
       m << decorated_label(name1, label_text, options)
       m << text_field(name1, options)
       m << text_field(name2, options)
