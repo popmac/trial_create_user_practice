1bd245e9695b1cc975aefb91d291eb00d4b67c3d
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-10 00:13:10 +0900

    職員アカウントの編集でパスワードを変更するかどうかを「変更する」ボタンをクリックするかどうかで決めるようにした。

---
+++ app/assets/javascripts/admin/staff_member_form.js
@@ -0,0 +1,14 @@
+$(function() {
+  $('form.edit_staff_member').on('click', '#enable-password-field', function() {
+    $('#enable-password-field').hide();
+    $('#disable-password-field').show();
+    $('#staff_member_password').removeAttr('disabled');
+    $('label[for=staff_member_password]').addClass('required');
+  });
+  $('form.edit_staff_member').on('click', '#disable-password-field', function() {
+    $('#disable-password-field').hide();
+    $('#enable-password-field').show();
+    $('#staff_member_password').attr('disabled', 'disabled');
+    $('label[for=staff_member_password]').removeClass('required');
+  });
+});
--- app/presenters/staff_member_form_presenter.rb
+++ app/presenters/staff_member_form_presenter.rb
@@ -3,6 +3,16 @@ class StaffMemberFormPresenter < FormPresenter
     if object.new_record?
       # 親クラスであるFormPresenterクラスのpassword_field_blockメソッドをオーバーライドしている
       super(name, label_text, options)
+    else
+      markup(:div, class: 'input-block') do |m|
+        m << decorated_label(name, label_text, options.merge(required: false))
+        m << password_field(name, options.merge(disabled: true))
+        m << ' '
+        m.button('変更する', type: 'button', id: 'enable-password-field')
+        m.button('変更しない', type: 'button', id: 'disable-password-field',
+          style: 'display: none')
+        m << error_messages_for(name)
+      end
     end
   end
 
