b6edb90904f502e0f378f069e312868420487983
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-09 22:27:39 +0900

    職員の氏名全体とフリガナ全体を生成するメソッドを追加した。

--- app/presenters/staff_member_presenter.rb
+++ app/presenters/staff_member_presenter.rb
@@ -3,6 +3,15 @@ class StaffMemberPresenter < ModelPresenter
   # 職員の停止フラグのOn/Offを表現する記号を返す
   #   On: BALLOT BOX WITH CHECK (U+2611)
   #   Off: BALLOT BOX (U+2610)
+
+  def full_name
+    object.family_name + ' ' + object.given_name
+  end
+
+  def full_name_kana
+    object.family_name_kana + ' ' + object.given_name_kana
+  end
+
   def suspended_mark
     # trueならば左側、falseなら右側の値が埋め込まれる
     # rawメソッドで&がエスケープ処理されないように制御している
