4e8aafd81887f85c0f68e0f51fcb9d311a89267a
popmac <matsukurahideki@matsukurahideki-no-MacBook-Pro.local>
2017-03-09 17:57:39 +0900

    Staff::ChangePasswordFormにバリデーションを追加した。

--- app/forms/staff/change_password_form.rb
+++ app/forms/staff/change_password_form.rb
@@ -4,8 +4,22 @@ class Staff::ChangePasswordForm
   attr_accessor :object, :current_password, :new_password,
     :new_password_confirmation
 
+  # 属性の名前に_confirmationを付加した名前を持つ属性とを比較して、値が一致しなければバリデーションが失敗する
+  validates :new_password, presence: true, confirmation: true
+
+  # 以下はカスタムバリデーションのカスタムメソッド
+  # ユーザーが入力した現在のパスワードが正しいかどうかをチェック
+  validate do
+    unless Staff::Authenticator.new(object).authenticate(current_password)
+      errors.add(:current_password, :wrong)
+    end
+  end
+
   def save
-    object.password = new_password
-    object.save
+    # バリデーションを実行して、trueならばsaveメソッドを実行
+    if valid?
+      object.password = new_password
+      object.save
+    end
   end
 end
