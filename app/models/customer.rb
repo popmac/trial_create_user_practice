b9835301a93820a0a4c4a7118ff55eba9c7209d6
popmac <parade8823@gmail.com>
2017-06-14 23:54:05 +0900

    モデル間で共通する記述をActiveSupport::Concernに抽出した。

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,8 @@
 class Customer < ApplicationRecord
+  include EmailHolder
+  include PersonalNameHolder
+  include PasswordHolder
+  
   has_one :home_address, dependent: :destroy, autosave: true
   has_one :work_address, dependent: :destroy, autosave: true
 
