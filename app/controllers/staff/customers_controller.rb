db37cdf26152af9f26846cc95ef97d4e5d4fa51c
popmac <parade8823@gmail.com>
2017-06-14 18:16:35 +0900

    Staff::Customersコントローラーのindexアクションを追加した。

--- app/controllers/staff/customers_controller.rb
+++ app/controllers/staff/customers_controller.rb
@@ -1,2 +1,6 @@
-class Staff::CustomersController < ApplicationController
+class Staff::CustomersController < Staff::Base
+  def index
+    @customers = Customer.order(:family_name_kana, :given_name_kana)
+      .page(params[:page])
+  end
 end
