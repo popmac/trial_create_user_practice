d5293fb77036d82b796fd0bf908231972afe33f3
popmac <parade8823@gmail.com>
2017-06-15 00:48:30 +0900

    顧客アカウントのフォームで入力フィールドの有効化・無効化をできるようにした。

--- app/assets/javascripts/staff/customer_form.js
+++ app/assets/javascripts/staff/customer_form.js
@@ -5,10 +5,34 @@ $( document ).on('turbolinks:load', function() {
     $('#form_customer_password').removeAttr('disabled');
     $('label[for=form_customer_password]').addClass('required');
   });
+
   $('form.edit_form').on('click', '#disable-password-field', function() {
     $('#disable-password-field').hide();
     $('#enable-password-field').show();
     $('#form_customer_password').attr('disabled', 'disabled');
     $('label[for=form_customer_password]').removeClass('required');
+  });
+
+  var toggle_home_address_fields = function() {
+    var checked = $('input#form_inputs_home_address').prop('checked');
+    $('fieldset#home-address-fields input').prop('disabled', !checked);
+    $('fieldset#home-address-fields select').prop('disabled', !checked);
+  };
+
+  var toggle_work_address_fields = function() {
+    var checked = $('input#form_inputs_work_address').prop('checked');
+    $('fieldset#work-address-fields input').prop('disabled', !checked);
+    $('fieldset#work-address-fields select').prop('disabled', !checked);
+  };
+
+  toggle_home_address_fields();
+  toggle_work_address_fields();
+
+  $('input#form_inputs_home_address').on('click', function() {
+    toggle_home_address_fields();
+  });
+
+  $('input#form_inputs_work_address').on('click', function() {
+    toggle_work_address_fields();
   });
 });
