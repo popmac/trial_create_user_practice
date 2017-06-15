7c70a0549de5b939bd5fe3eac478faf63ff2d94e
popmac <parade8823@gmail.com>
2017-06-15 18:48:08 +0900

    インデントを調整した。

--- app/forms/staff/customer_form.rb
+++ app/forms/staff/customer_form.rb
@@ -61,22 +61,22 @@ class Staff::CustomerForm
   private
   def customer_params
     @params.require(:customer).permit(
-    :email, :password,
-    :family_name, :given_name, :family_name_kana, :given_name_kana,
-    :birthday, :gender
+      :email, :password,
+      :family_name, :given_name, :family_name_kana, :given_name_kana,
+      :birthday, :gender
     )
   end
 
   def home_address_params
     @params.require(:home_address).permit(
-    :postal_code, :prefecture, :city, :address1, :address2,
+      :postal_code, :prefecture, :city, :address1, :address2,
     )
   end
 
   def work_address_params
     @params.require(:work_address).permit(
-    :postal_code, :prefecture, :city, :address1, :address2,
-    :company_name, :division_name
+      :postal_code, :prefecture, :city, :address1, :address2,
+      :company_name, :division_name
     )
   end
 
