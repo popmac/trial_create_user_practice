84952cc800cc9cce4797b048aa27c86f76faa496
popmac <parade8823@gmail.com>
2017-06-16 20:01:35 +0900

    Phonesテーブルの既存レコードに対して、カラムlast_four_digitsの値を更新するマイグレーションを追加した。

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170616105345) do
+ActiveRecord::Schema.define(version: 20170616105803) do
 
   create_table "addresses", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "customer_id",                null: false
