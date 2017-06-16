ab6d63ddf8a3e1889e7fa902af93650bcac9c073
popmac <parade8823@gmail.com>
2017-06-16 19:30:44 +0900

    検索機能を追加した。

--- app/controllers/staff/customers_controller.rb
+++ app/controllers/staff/customers_controller.rb
@@ -1,8 +1,7 @@
 class Staff::CustomersController < Staff::Base
   def index
-    @search_form = Staff::CustomerSearchForm.new
-    @customers = Customer.order(:family_name_kana, :given_name_kana)
-      .page(params[:page])
+    @search_form = Staff::CustomerSearchForm.new(search_params)
+    @customers = @search_form.search.page(params[:page])
   end
 
   def show
@@ -46,5 +45,15 @@ class Staff::CustomersController < Staff::Base
     customer.destroy!
     flash.notice = '顧客アカウントを削除しました。'
     redirect_to :staff_customers
+  end
+
+  private
+  def search_params
+    if params[:search].present?
+      params.require(:search).permit(
+        :family_name_kana, :given_name_kana,
+        :birth_year, :birth_month, :birth_mday,
+        :address_type, :prefecture, :city, :phone_number)
+    end
   end
 end
